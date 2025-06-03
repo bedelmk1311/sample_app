ce85fed45990da1bbf75a9cf19a5c44584e03966
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-06-03 07:42:25 +0000

    [add]list/create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,6 +3,12 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to homes_top_path
+  end
+
   def index
   end
 
@@ -10,5 +16,16 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  # privateは一種の境界線で、ここから下はこのcontrollerの中でしか呼び出せませんの意味
+  # privateメソッドの名前は、「モデル名_params」とすることが多いです
+
+  # ストロングパラメータ
+  # 保存できるカラムを限定する時に使う
+  # ここではlistモデルのtitleとbodyの保存を許可する
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
