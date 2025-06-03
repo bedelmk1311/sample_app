1b88750ac79869a217baaf6dda5ddb74940de211
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-06-03 07:25:22 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
+  # get 'homes/top' => 'homes#top'
+  # URLと'コントローラ名#アクション名'の形が同じになる場合は
+  # 'コントローラ名#アクション名'の部分を省略することができます
+
 Rails.application.routes.draw do
+  get 'homes/top'
   get 'lists/new'
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
-  get 'homes/top'
+
+  post 'lists' => 'lists#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
